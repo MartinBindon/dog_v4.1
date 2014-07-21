46efc6a7b6a3e25500ec29c52cda3a5fa7ec4c76
MartinBindon <binm1_11@uni.worc.ac.uk>
2014-07-21 01:29:47 +0100

    4.3
    LocalStorage Create and Save working.

--- js/localStorage.js
+++ js/localStorage.js
@@ -1,31 +1,63 @@
-$(document).ready(function() {
-	if (typeof(localStorage) == 'undefined' ) {
-		alert('Your browser does not support HTML5 local storage.');
-		} else {
-		getPet(); // load the pet details
+// Global variables
 
-		
-		}
+
+	var tbPets = localStorage.getItem("tbPets");  // create tbPets variable and attempt to retrieve any data from it
+	tbPets = JSON.parse(tbPets); // convert data to object
+
+// if tbPets does not exist create an array to populate
+	if (tbPets == null) {
+		tbPets = [];
 	}
-})
 
-function addPetName(this) {
-	var newPetName = JSON.stringify(this);
-	window.localStorage.setItem("pet_name", newPetName);
 
-}
 
-function clearPet() {
-	localStorage.removeItem("pet_name");
-	localStorage.removeItem("pet_photo");
-	localStorage.removeItem("pet_breed");
-	localStorage.removeItem("pet_age");
-	localStorage.removeItem("pet_wt");
-	localStorage.removeItem("vet_name");
-	localStorage.removeItem("vet_number");
-	localStorage.removeItem("vet_oohcontact");
-	localStorage.removeItem("ins_name");
-	localStorage.removeItem("ins_policyNo");
-	localStorage.removeItem("ins_contact");
-}
-		
+
+function addPetName(val) 
+    {
+        var newPetName = JSON.stringify(val);
+        localStorage.setItem("pet_name", newPetName);
+    }
+
+
+function clearPet() 
+	{
+		localStorage.removeItem("pet_name");
+		localStorage.removeItem("pet_photo");
+		localStorage.removeItem("pet_breed");
+		localStorage.removeItem("pet_age");
+		localStorage.removeItem("pet_wt");
+		localStorage.removeItem("vet_name");
+		localStorage.removeItem("vet_number");
+		localStorage.removeItem("vet_oohcontact");
+		localStorage.removeItem("ins_name");
+		localStorage.removeItem("ins_policyNo");
+		localStorage.removeItem("ins_contact");
+	}
+
+function getPet() 
+	{
+		console.log("getPet called.");
+	}
+
+// Adds a pet to the JSON array, creates array if no array exists (first time it is clicked)
+function createPet() {
+
+	// create array and add selected pet
+
+	var pets = JSON.stringify({
+		//ID : $("#petID").val(),
+		Name : $("#textinput1").val(),
+		Breed : $("#textinput3").val()
+	});
+
+	tbPets.push(pets);
+	localStorage.setItem("tbPets", JSON.stringify(tbPets));
+	console.log("the data was sarved");
+
+
+
+	// else add new pet to existing array (push?)
+
+	// add the pet to the array
+
+}	
